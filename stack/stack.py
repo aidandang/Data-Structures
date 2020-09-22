c147eee3ab99f0623771be66a0284bc8ba2bd8cd
Aidan Dang <aidandang@gmail.com>
2020-09-22 11:48:56 -0700

    added Stack with LinkedList() and tested ok

--- stack/stack.py
+++ stack/stack.py
@@ -11,19 +11,41 @@ return elements in Last In First Out order.
    implementing a Stack?
 """
 
+# class Stack:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = []
+
+#     def __len__(self):
+#         return len(self.storage)
+
+#     def push(self, value):
+#         return self.storage.append(value)
+
+#     def pop(self):
+#         if len(self.storage) == 0:
+#             return None
+#         else:
+#             return self.storage.pop()
+
+from singly_linked_list import LinkedList
+
 class Stack:
     def __init__(self):
         self.size = 0
-        self.storage = []
+        self.storage = LinkedList()
 
     def __len__(self):
-        return len(self.storage)
+        return self.size
 
     def push(self, value):
-        return self.storage.append(value)
+        self.storage.add_to_tail(value)
+        self.size += 1
 
     def pop(self):
-        if len(self.storage) == 0:
+        if self.size == 0:
             return None
-        else:
-            return self.storage.pop()
+        self.size -= 1
+        return self.storage.remove_tail()
+        
+
